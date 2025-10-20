725f328dec6b4250132739e4e59e2bc2143937b6
Noelia Noemí González <gonzaleznoelia158@gmail.com>
2025-10-20 17:02:06 -0300

    Update Materia.java

--- TP1.java/Materia.java
+++ TP1.java/Materia.java
@@ -1,10 +1,14 @@
 public class Materia{
+    //Atributos
     private String nombre;
     private String codigo;
     private int creditos;
     private double calificacion;
     private Profesor profesor;
+
+    //Constructor vacío
     public Materia(){}
+    //Constructor con parámetros
     public Materia(String nombre, String codigo, int creditos, double calificacion, Profesor profesor){
     this.nombre = nombre;
     this.codigo = codigo;
@@ -12,16 +16,21 @@ public class Materia{
     this.calificacion = calificacion;
     this.profesor = profesor;
 }
+    //Métodos getter
     public String getNombre(){return nombre;}
     public String getCodigo(){return codigo;}
     public int getCreditos(){return creditos;}
     public double getCalificacion(){return calificacion;}
     public Profesor getProfesor(){return profesor;}
+
+    //Métodos setter
     public void setNombre(String nombre){this.nombre = nombre;}
     public void setCodigo(String codigo){this.codigo = codigo;}
     public void setCreditos(int creditos){this.creditos = creditos;}
     public void setCalificacion(double calificacion){this.calificacion = calificacion;}
     public void setProfesor(Profesor profesor){this.profesor = profesor;}
+
+    //equals()
     @Override
     public boolean equals(Object o){
         if(this == o) return true;
@@ -29,6 +38,7 @@ public class Materia{
         Materia materia = (Materia) o;
         return codigo.equals(materia.codigo);
     }
+    //hashCode()
     @Override
     public int hashCode(){
         return codigo.hashCode();
@@ -38,4 +48,3 @@ public class Materia{
     {return "Materia [nombre: "+ nombre + ". codigo: "+ codigo + ". Creditos: "+ 
     creditos + ". Calificacion: " + calificacion + ". Profesor: "+ profesor +"]";}
 }
-//Consultar el tema de profesor si asi esta bien.....
