0096e41f328cc818d89ee097bfb9900c517e4b17
Noelia Noemí González <gonzaleznoelia158@gmail.com>
2025-10-20 17:16:19 -0300

    Update Personal.java

--- TP1.java/Personal.java
+++ TP1.java/Personal.java
@@ -1,19 +1,27 @@
 public class Personal extends Persona implements MiembroUniversidad{
+    //Atributos
     private String departamento;
     private String puesto;
     private String fechaIngreso;
+
+    //Métodos getter
     public String getDepartamento(){return departamento;}
     public String getPuesto(){return puesto;}
     public String getFechaIngreso(){return fechaIngreso;}
+
+    //Métodos setter
     public void setDepartamento(String departamento){this.departamento = departamento;}
     public void setPuesto(String puesto){this.puesto = puesto;}
     public void setFechaIngreso(String fechaIngreso){this.fechaIngreso = fechaIngreso;}
+
+    //Constructor
     public Personal(String nombre, String apellido,int edad, String documento, String departamento, String puesto, String fechaIngreso){
         super(nombre, apellido, edad, documento);
         this.departamento = departamento;
         this.puesto = puesto;
         this.fechaIngreso = fechaIngreso;
     }
+    //Interfaz
     @Override
     public String toString(){
         return "Personal: ("+ super.toString() + ". Departamento: " + departamento + ". Puesto: "+ puesto + ". Fecha de ingreso: "+ fechaIngreso+ ")";
@@ -26,4 +34,5 @@ public class Personal extends Persona implements MiembroUniversidad{
     public String obtenerInformacionCompleta(){
         return this.toString();
     }
+
     }
